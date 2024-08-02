ff5c7080b49cd80c10c724b0fd57e9702e3f5033
diiegotag <diiegotag@gmail.com>
2024-08-02 03:45:18 -0700

    Product Details finished

--- ProjectPro/app/components/productDetails/productDetails.js
+++ ProjectPro/app/components/productDetails/productDetails.js
@@ -1,6 +1,11 @@
 import { getProducts } from "../../js/providers/products.js";
 import Swal from 'https://cdn.skypack.dev/sweetalert2';
 
+import { toggleContent } from "../sidemenu/sidemenu.js";
+
+import { menu } from "../sidemenu/settings.js";
+
+
 export const init = () => {
 
   let product = JSON.parse(localStorage.getItem('productDetails')) || {};
@@ -20,6 +25,7 @@ function showProductDetails(product) {
     
     productDetails.querySelector('#product-name').textContent = product.productName;
     productDetails.querySelector('#product-price').textContent = "$"+product.price;
+    productDetails.querySelector('#product-description').textContent = product.description;
 
     console.log("https://arcticbreeze.blob.core.windows.net/productocontenedor/"+product.productImage);
     if(product.productImage == null || product.productImage == ""){
@@ -27,4 +33,45 @@ function showProductDetails(product) {
     }else{
       productDetails.querySelector('#product-img').src = "https://arcticbreeze.blob.core.windows.net/productocontenedor/"+product.productImage;
     }
+
+    let backbutton = productDetails.querySelector('#back-button');
+    backbutton.dataset.productId = product.productID;
+
+    backbutton.addEventListener('click', (e) => {
+      toggleContent(menu[1]);
+    });
+
+
+    let button = productDetails.querySelector('#cart-button');
+    button.dataset.productId = product.productID;
+
+    button.addEventListener('click', (e) => {
+      let productId = e.currentTarget.dataset.productId;
+      handleProductSelection(productId);
+    });
 }
+
+function handleProductSelection(productId) {
+  Swal.fire({
+    title: "Add to your cart?",
+    showDenyButton: true,
+    confirmButtonText: "Add to cart",
+    denyButtonText: `Cancel`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let counts = JSON.parse(localStorage.getItem('productCounts')) || {};
+
+      if (counts[productId]) {
+        counts[productId]++;
+      } else {
+        counts[productId] = 1;
+      }
+    
+      localStorage.setItem('productCounts', JSON.stringify(counts));
+    
+      console.log(`Product ${productId} selected ${counts[productId]} times.`);
+      Swal.fire("Product Added!", "", "success");
+    } 
+  });
+}
+
